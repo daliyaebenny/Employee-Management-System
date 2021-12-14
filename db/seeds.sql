2d948a3305693fcbf9e808165664e47d95a3ca0b
Daliya Benny <daliyaebenny@gmail.com>
2021-12-14 13:05:37 -0800

    Seeds file for department and role

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES ("Finance"),
+       ("R&D"),
+       ("Production"),
+       ("Human ResourceManagement"),
+       ("Marketting");
+
+INSERT INTO role (title, salary,dept_id)
+VALUES ("Manager",12000,1),
+       ("Assistant",2000,2),
+       ("Clerk",5000,2),
+       ("Manager",12000,3),
+       ("Engineer",10000,2),
+       ("Worker",1000,1);
+
+    
