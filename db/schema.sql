f8354ea23b426411e39de812cb8fb47629601cde
Daliya Benny <daliyaebenny@gmail.com>
2021-12-14 13:03:34 -0800

    Added db schema file

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS ems_db;
+CREATE DATABASE ems_db;
+
+USE ems_db;
+
+CREATE TABLE department(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name  VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INT,
+    FOREIGN KEY (dept_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname DECIVATCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+	manager_id INT,
+    FOREIGN KEY(role_id) REFERENCES role(id) ,
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL 
+);
