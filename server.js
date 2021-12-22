1d99d2005bb2320d91ea0fc7b04cc20e817a1ccb
Daliya Benny <daliyaebenny@gmail.com>
2021-12-21 16:24:45 -0800

    Added function to Add a department

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
-const express = require('express');
 // Import and require mysql2
+const express = require('express');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -23,31 +22,57 @@ const db = mysql.createConnection(
   console.log(`Connected to the books_db database.`)
 );
 
-
-
-
+//viewAllEmployees 
 function viewAllEmployees(){
     db.query('SELECT * FROM employee', function (err, results) {
         const table = cTable.getTable(results);
         console.log(table);
         });
 }
-
+// viewAllRoles
 function viewAllRoles(){
     db.query('SELECT * FROM roles', function (err, results) {
         const table = cTable.getTable(results);
         console.log(table);
     });
 }
+
+// viewAllDepartments
 function viewAllDepartments(){
     db.query('SELECT * FROM department', function (err, results) {
         const table = cTable.getTable(results);
         console.log(table);
         });
 }
-viewAllEmployees();
-viewAllRoles();
-viewAllDepartments();
+
+function addDepartment(seedData){
+    
+    console.log(`INSERT INTO department (name) VALUES (${seedData.dept_name})`);
+    db.query(`INSERT INTO department (name) VALUES (?)`,seedData.dept_name,function (err, results,) {
+        console.log(results);
+    });
+}
+
+// function addDepartment(){
+// // TODO
+// }
+
+//  function addDepartment(){
+// // TODO
+// }
+
+// viewAllEmployees();
+// viewAllRoles();
+// viewAllDepartments();
+
+
+const tempData = {
+    dept_name:'Trial'
+}
+console.log('before call');
+addDepartment(tempData);
+
+
 app.use((req, res) => {
   res.status(404).end();
 });
